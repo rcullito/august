1f8dd5e555083b1965815f4dd9c8f905af80c2b5
Rob Culliton <rob.culliton@gmail.com>
2021-07-26 09:25:40 -0400

    defun o!-symbol-p and o!-symbol-to-g!-symbol

--- hoyte.lisp
+++ hoyte.lisp
@@ -110,6 +110,19 @@
               syms)
          ,@body))))
 
+(defun o!-symbol-p (s)
+  (and (symbolp s)
+       (> (length (symbol-name s)) 2)
+       (string= (symbol-name s)
+                "O!"
+                :start1 0
+                :end1 2)))
+
+(defun o!-symbol-to-g!-symbol (s)
+  (symb "G!"
+        (subseq (symbol-name s) 2)))
+
+
 
 `(football-game
  (game-started-at
