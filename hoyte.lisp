2b8cb05f2ff75bcdc403659ac6bf1c4b91a515ab
Rob Culliton <rob.culliton@gmail.com>
2021-07-26 08:46:22 -0400

    core material included in hotye, up until g!

--- hoyte.lisp
+++ hoyte.lisp
@@ -35,7 +35,6 @@
                        (rec (cdr x) acc)))))) ;; beware, double call to rec within one form!
     (rec x nil))) 
 
-(flatten '(1 2 (3 4) 5))
 
 (defun block-scanner (trigger-string)
   (let* ((trig (coerce trigger-string 'list))
@@ -74,9 +73,58 @@
               (segment-reader stream ch (- n 1))))))
 
 
-(segment-reader t #\/ 3)
+(defmacro nlet (n letargs &rest body)
+  `(labels ((,n ,(mapcar #'car letargs)
+              ,@body))
+     (,n ,@(mapcar #'cadr letargs))))
 
 
-(char= #\a #\b)
+(defun nlet-fact (n)
+  (nlet fact
+        ((n n))
+        (if (zerop n)
+            1
+            (* n (fact (1- n))))))
+
+
+(defun g!-symbol-p (s)
+  (and (symbolp s)
+       (> (length (symbol-name s)) 2)
+       (string= (symbol-name s)
+                "G!"
+                :start1 0
+                :end1 2)))
+
+
+
+(defmacro defmacro/g! (name args &rest body)
+  (let ((syms (remove-duplicates
+               (remove-if-not #'g!-symbol-p
+                              (flatten body)))))
+    `(defmacro ,name ,args
+       (let ,(mapcar
+              (lambda (s)
+                `(,s (gensym ,(subseq
+                               (symbol-name s)
+                               2))))
+              syms)
+         ,@body))))
+
+
+;; (defmacro/g! nif (expr pos zero neg)
+;;   `(let ((,g!result ,expr))
+;;      (cond ((plusp ,g!result) ,pos)
+;;            ((zerop ,g!result) ,zero)
+;;            (t ,neg))))
+
+;; (nif 25 'pos 'zero 'neg)
+
+;; (gensym "result")
+
+
+;; (segment-reader t #\/ 3)
+
+
+;; (char= #\a #\b)
 
 
