a3457dc517ab9be7d6ba2886b2c2c6c5c928efa5
Rob Culliton <rob.culliton@gmail.com>
2021-08-11 13:39:34 -0400

    include a description of what each component of the continuation is responsible for

--- continuity.scm
+++ continuity.scm
@@ -3,6 +3,12 @@
 
 (define frozen)
 
+;; call-with-current-continuation takes a function of one argument,
+;; that argument will represent the current continuation
+
+;; by storing the value of that continuation, we save the state of computation
+;; at the point of the outer call-with-current-continuation
+
 (append '(the continuation example returned)
         (list (call-with-current-continuation
                (lambda (cc)
