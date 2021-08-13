668989352c6b7a9a38563ec35a23461167e58e6a
Rob Culliton <rob.culliton@gmail.com>
2021-08-13 07:35:28 -0400

    add additional comments on the approach of nondeterministic search relative to deterministic search

--- nondeterminism.scm
+++ nondeterminism.scm
@@ -1,5 +1,10 @@
 
+;; A deterministic search program could take a query and generate
+;; all the solutions which satisfied it
 
+;; A nondeterministic search program will use choose to generate
+;; solutions one at a time, and if more are needed, will call
+;; fail to restart the search
 
 
 (define *paths* ())
